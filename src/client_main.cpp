46587aa03902bcd2a0ec4f40ceb6a7d00077ff4c
Colin0625 <sepulveda.colin.m@gmail.com>
2026-03-28 19:27:11 -0500

    changed ip

--- src/client_main.cpp
+++ src/client_main.cpp
@@ -3,7 +3,7 @@
 int main() {
     Client client;
     std::string file = "cpp.png";
-    client.connect_to_server(5000, "127.0.0.1");
+    client.connect_to_server(5000, "100.83.112.113");
     client.request_file(file);
     return 1;
 }
