5e286fc52438d5da13930023f6c7584879b9c8d6
Colin Sepulveda <sepulveda.colin.m@gmail.com>
2026-04-25 01:06:56 -0500

    removed debug

--- src/net/UdpSocket.cpp
+++ src/net/UdpSocket.cpp
@@ -21,7 +21,6 @@ void UdpSocket::bind_socket(const SocketAddress& addr) {
 }
 
 ssize_t UdpSocket::send_to(std::span<const std::byte> buffer, const SocketAddress& addr) {
-    std::cout << "sending packet from UdpSocket::send_to" << std::endl;
     const std::byte* payload = reinterpret_cast<const std::byte*>(buffer.data());
     ssize_t sent = sendto(socket_fd_, payload, buffer.size(), 0, addr.data(), addr.size());
     if (sent < 0) {
